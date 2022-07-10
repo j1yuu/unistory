1d2cd4a8f58aba6652b9443e76237eccba1f2428
Konstantin <o.lover4@bk.ru>
2022-07-10 19:07:43 +0300

    1.0.8--1

--- components/setId.js
+++ components/setId.js
@@ -6,16 +6,6 @@ export const settingId = (num) => {
         ids.push(item.id)
     })
 
-    // let a = false
-    // while (a == false) {
-    //     let id = Math.floor(Math.random() * num)
-    //     let b = 1
-    //     for (var i = 0; i < ids.length; i++) {
-    //         if (id == ids.lenth) break
-    //         b = b + 1
-    //     }
-    //     if (b == ids.length) return id, (a = true)
-    // }
     let x = false
     while (x == false) {
         let id = Math.floor(Math.random() * num)
